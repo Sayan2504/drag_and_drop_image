8582c0cea63ac9aaabb9fb4b7ed404fb55fda310
Sayan Sinha <sinhasayan888@gmail.com>
2020-06-09 00:34:42 +0530

    adding moveable

--- src/Components/Images/images.js
+++ src/Components/Images/images.js
@@ -1,10 +1,8 @@
 import React, { useEffect, useState, useRef } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Row, Col, Card, Image } from "react-bootstrap";
+import { Container, Row, Col, Card, Form, Button, Image } from "react-bootstrap";
 import Selecto from "react-selecto";
 import Moveable from "react-moveable";
-import SingleImage from "./image";
-import update from "immutability-helper";
 import "./images.css";
 
 const Images = ({ imgData }) => {
@@ -14,7 +12,7 @@ const Images = ({ imgData }) => {
   const moveableRef = useRef(null);
   const selectoRef = useRef(null);
 
-  /*useEffect(() => {
+  useEffect(() => {
     let temp = [];
     imgData.forEach((item) => {
       item.images.forEach((image) => {
@@ -27,45 +25,14 @@ const Images = ({ imgData }) => {
       });
     });
     setImages(temp);
-  }, [imgData]);*/
-
-  useEffect(() => {
-    createImage(imgData);
   }, [imgData]);
 
   useEffect(() => {
     console.log(images);
   }, [images]);
 
-  const moveImage = (dragIndex, hoverIndex) => {
-    const draggedImage = images[dragIndex];
-    setImages(
-      update(images, {
-        $splice: [
-          [dragIndex, 1],
-          [hoverIndex, 0, draggedImage],
-        ],
-      })
-    );
-  };
-
-  const createImage = (imgData) => {
-    let temp = [];
-    imgData.forEach((item) => {
-      item.images.forEach((image) => {
-        let t = {
-          key: image.text,
-          photo: image,
-          description: item.description,
-        };
-        temp.push(t);
-      });
-    });
-    setImages(temp);
-  }
-
-  const imgList = images.map((img, index) => (
-    /*<Col sm={3} className="mt-3 imageCube">
+  const imgList = images.map((image) => (
+    <Col sm={3} className="mt-3 imageCube">
       <div className="hvrbox">
         <Card>
           <Card.Header>
@@ -81,31 +48,99 @@ const Images = ({ imgData }) => {
           </div>
         </div> 
       </div>
-    </Col>*/
-    <SingleImage image={img} moveImage={moveImage} index={index} selectoRef={selectoRef} />
+    </Col>
   ));
 
   return (
     <Container className="p-5 elements selecto-area">
-      <Selecto
-        selectoref={selectoRef}
-        dragContainer={".elements"}
-        selectableTargets={[".selecto-area .imageCube"]}
-        hitRate={90}
-        selectByClick={true}
-        selectFromInside={false}
-        toggleContinueSelect={["shift"]}
-        onSelect={(e) => {
-          e.added.forEach((el) => {
-            el.classList.add("selected");
-            createImage(imgData);
-          });
-          e.removed.forEach((el) => {
-            el.classList.remove("selected");
-          });
-        }}
-      />
-      <Row>{imgList}</Row>
+      <Row>
+        <Moveable
+          ref={moveableRef}
+          draggable={true}
+          target={targets}
+          onClickGroup={(e) => {
+            selectoRef.current.clickTarget(e.inputEvent, e.inputTarget);
+          }}
+          
+          onDragStart={(e) => {
+            const target = e.target;
+            if (!frameMap.has(target)) {
+              frameMap.set(target, {
+                translate: [0, 0],
+              });
+            }
+            const frame = frameMap.get(target);
+            e.set(frame.translate);
+          }}
+
+          onDrag={(e) => {
+            const target = e.target;
+            const frame = frameMap.get(target);
+            frame.translate = e.beforeTranslate;
+            target.style.transform = `translate(${frame.translate[0]}px, ${frame.translate[1]}px)`;
+          }}
+
+          onDragGroupStart={(e) => {
+            e.events.forEach((ev) => {
+              const target = ev.target;
+              if (!frameMap.has(target)) {
+                frameMap.set(target, {
+                  translate: [0, 0],
+                });
+              }
+              const frame = frameMap.get(target);
+              ev.set(frame.translate);
+            });
+          }}
+
+          onDragGroup={(e) => {
+            e.events.forEach((ev) => {
+              const target = ev.target;
+              const frame = frameMap.get(target);
+              frame.translate = ev.beforeTranslate;
+              target.style.transform = `translate(${frame.translate[0]}px, ${frame.translate[1]}px)`;
+            });
+          }}
+
+          onDragEnd={({ target, isDrag, clientX, clientY }) => {
+            console.log("onDragEnd", target, isDrag);
+          }}
+
+        ></Moveable>
+        <Selecto
+          ref={selectoRef}
+          dragContainer={".elements"}
+          selectableTargets={[".selecto-area .imageCube"]}
+          hitRate={90}
+          selectByClick={true}
+          selectFromInside={true}
+          toggleContinueSelect={["shift"]}
+          onDragStart={(e) => {
+            const moveable = moveableRef.current;
+            const target = e.inputEvent.target;
+            if (
+              moveable.isMoveableElement(target) ||
+              targets.some((t) => t === target || t.contains(target))
+            ) {
+              e.stop();
+            }
+          }}
+          onSelectEnd={(e) => {
+            const moveable = moveableRef.current;
+            setTargets(e.selected);
+
+            if (e.isDragStart) {
+              e.inputEvent.preventDefault();
+
+              setTimeout(() => {
+                moveable.dragStart(e.inputEvent);
+              });
+            }
+          }}
+
+        ></Selecto>
+        {imgList}
+      </Row>
     </Container>
   );
 };
